d83da44f322c29bce6c0c89e0a4e94000e59f89e
knkr0203 <masked_universe@yahoo.co.jp>
2019-04-25 15:35:44 +0900

    Change redirect function and add flash messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      flash[:success] = "Login successed."
       redirect_back_or root_url
     else
       flash.now[:danger] = 'Invalid email/password combination'
